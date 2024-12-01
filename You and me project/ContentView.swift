a3912f535995a173b572d5db2d7e9d93a2d88d84
miki1981-max <miki43154@gmail.com>
2024-12-01 12:44:49 +0100

    Initial Commit

---
+++ You and me project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  You and me project
+//
+//  Created by Kamila Ponomarova on 2024-12-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ You and me project/You_and_me_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  You_and_me_projectApp.swift
+//  You and me project
+//
+//  Created by Kamila Ponomarova on 2024-12-01.
+//
+
+import SwiftUI
+
+@main
+struct You_and_me_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
