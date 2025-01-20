6977d26c38a23e5fdb9bf143405ad15b6709989e
BreakinBenny <fox@blaj.net>
2025-01-20 14:06:20 +0100

    Commit from GitHub (#1)
    I'm having a bit of trouble with the Git feature in Xcode, so I'm doing this change directly from the GitHub website… hope that's OK.

--- You and me project/RecommendationsView.swift
+++ You and me project/RecommendationsView.swift
@@ -10,33 +10,78 @@ import SwiftUI
 struct RecommendationsView: View {
     var body: some View {
         NavigationView {
-        VStack {
-            Text("Recommendations for a person who uses a cane or walkers. The steps described below can be done either by a sick person independently or with the help of an assistant.")
-            NavigationLink(destination: Fillinginformation1()) {
-                Text("Morning")
-            
-                .font(.body)
-                .padding()
-            
-                    
-            }
-        
-            .frame(maxWidth: 800, maxHeight: 550)
-            .background(Color.background)
-            
             VStack {
-                Text("Write the care receiver’s symptoms into a diary")
-                    
+                Text("Recommendations for a person who uses a cane or walkers. The steps described below can be done either by a sick person independently or with the help of an assistant.")
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                ScrollView {
+                    VStack {
+                        NavigationLink(destination: Fillinginformation1()) {
+                            Text("Morning")
+                                .font(.largeTitle)
+                        }
+                        Text("Write the care receiver's symptoms into a diary...")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                        
+                        NavigationLink(destination: Fillinginformation1()) {
+                            Text("Comfort and safety ")
+                                .font(.largeTitle)
+                        }
+                        .padding()
+                        
+                        Text("Check whether the care receiver's in comfortable condition and feels safe. Make sure you follow all steps.")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                        
+                        NavigationLink(destination: Fillinginformation1()) {
+                            Text("Hygiene")
+                                .font(.largeTitle)
+                        }
+                        .padding()
+                        
+                        Text("Hygiene plays an extremely important role in the life of a person with diabetes. Make sure you follow all steps.")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                        
+                        NavigationLink(destination: Fillinginformation1()) {
+                            Text("Intimate hygiene")
+                                .font(.largeTitle)
+                        }
+                        .padding()
+                        
+                        Text("It's important that the care receiver takes all medications recommended by the doctor strictly at the time they were prescribed.")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                        
+                        NavigationLink(destination: Fillinginformation1()) {
+                            Text("Nutrition")
+                                .font(.largeTitle)
+                        }
+                        .padding()
+                        
+                        Text("A properly balanced diet is one of the main components of therapy for diabetes.")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                        
+                        NavigationLink(destination: Fillinginformation1()) {
+                            Text("Benefits of walking")
+                                .font(.largeTitle)
+                        }
+                        .padding()
+                        
+                        Text("It's important that the care receiver takes all medications recommended by the doctor strictly at the time they were prescribed.")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                    }
+                    .frame(maxWidth: 800, maxHeight: 900)
+                    .background(Color.background)
                 }
             }
-            .frame(maxWidth: 800, maxHeight: 800)
-            .background(Color.white)
+            .background(Color.background)
         }
-        
-        }
-        }
-    
-    
+    }
+}
 
 #Preview {
     RecommendationsView()
