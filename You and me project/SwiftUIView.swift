b18cbce6a5f54a5bba8b9c5a84ba9ead2ee545fa
miki1981-max <miki43154@gmail.com>
2025-01-25 13:05:32 +0100

    v 1.00.17

---
+++ You and me project/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  You and me project
+//
+//  Created by Kamila Ponomarova on 2025-01-25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("aaaaaaaaaa")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
